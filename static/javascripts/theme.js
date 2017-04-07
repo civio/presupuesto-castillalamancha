1da642906051cef66d432161978efdf981971656
Eduardo Sebastian <esebastian@altiora.es>
2017-04-07 11:05:51 +0200

    Include explanatory note in overview section
    To explain the fact that the sankey graph only includes the non
    financial income and expenses (chapters I to VII).

--- static/javascripts/theme.js
+++ static/javascripts/theme.js
@@ -1,5 +1,15 @@
 // Theme custom js methods
 $(document).ready(function(){
 
-  // Add here your custom code
+  var addSankeyAlert = function(selector) {
+    var str = {
+      'es': 'Se representan tanto los ingresos como los gastos no financieros (Capítulos del I al VII)',
+    };
+    var cont = $(selector);
+    if (cont.size() > 0) {
+      cont.prepend('<div class="alert alert-data-update">' + str[$('html').attr('lang')] + '</div>');
+    }
+  };
+
+  addSankeyAlert('.sankey-container');
 });
