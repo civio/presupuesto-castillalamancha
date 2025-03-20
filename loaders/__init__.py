4a3b93da89231c07c0f38729c5620c0a0cd7d72e
David Cabo <david.cabo@gmail.com>
2025-03-20 22:20:23 +0100

    Use Python 3-compatible imports civio/presupuesto-management#1286

--- loaders/__init__.py
+++ loaders/__init__.py
@@ -1,1 +1,6 @@
-from castilla_la_mancha_budget_loader import CastillaLaManchaBudgetLoader
+import six
+
+if six.PY2:
+    from castilla_la_mancha_budget_loader import CastillaLaManchaBudgetLoader
+else:
+    from .castilla_la_mancha_budget_loader import CastillaLaManchaBudgetLoader
